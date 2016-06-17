7668e019c3d6a7ea77d798ebcb5d00c8e8d1ade2
CapacitorSet <capacitorset@users.noreply.github.com>
2016-06-17 22:55:20 +0200

    Added proxied objects

--- run.js
+++ run.js
@@ -13,9 +13,8 @@ fs.writeFileSync("urls.json", JSON.stringify(urls, null, '\t'));
 fs.writeFileSync("snippets.json", JSON.stringify(snippets, null, '\t'));
 fs.writeFileSync("resources.json", JSON.stringify(resources, null, '\t'))
 
-function evaluator(code, v8Context) {
+function evaluator(code) {
 	var self = this;
-	this.context = null;
 	code = "1;" + code; // For whatever reason, it prevents many parsers from breaking
 	var filename = uuid.v4() + ".js";
 	console.log("Code saved to", filename);
@@ -23,12 +22,12 @@ function evaluator(code, v8Context) {
 	fs.writeFileSync(filename, beautify("1;" + code));
 
 	safe("1;" + code, {
-		//eval: code => evaluator(code, self.context),
+		//eval: code => evaluator(code),
 		console: {
 			log: x => console.log(x)
 		},
 		ActiveXObject: function(name) {
-			//console.log("New ActiveXObject created:", name);
+			console.log("New ActiveXObject created:", name);
 			switch (name) {
 				case "WScript.Shell": {
 					this.ExpandEnvironmentStrings = function(arg) {
@@ -46,77 +45,113 @@ function evaluator(code, v8Context) {
 					}				
 				}
 				break;
-				case "MSXML2.XMLHTTP": {
-					this.open = function(method, url) {
-						console.log("Request made:", method, url);
-						urls.push(url);
-						this.url = url;
-					}
-					this.setRequestHeader = function(key, val) {
-						console.log("Header set:", key, val);
-					}
-					this.send = function(data) {
-						if (data)
-							console.log("Data sent:", data);
-						this.readyState = 4;
-						this.status = 200;
-						this.ResponseBody = `(Content of ${this.url})`;
-						this.onreadystatechange();
-					}
-				}
+				case "MSXML2.XMLHTTP":
+					return new ProxiedXMLHTTP();
 				break;
-				case "ADODB.Stream": {
-					this.buffer = "";
-					this.open = () => {}
-					this.Open = this.open;
-					var resourcename = uuid.v4();
-					this.Write = this.write = function(chunk) {
-						this.buffer += String(chunk);
-					}
-					this.SaveToFile = this.saveToFile = function(filename) {
-						this.virtual_filename = filename;
-					}
-					this.Close = this.close = () => {
-						resources[resourcename] = {as: this.virtual_filename};
-						console.log("ADODB stream created:", resourcename);
-						fs.writeFileSync(resourcename, this.buffer);
-					}
-				}
-				break;
+				case "ADODB.Stream":
+					return new ProxiedADODBStream();
+				case "Msxml2.DOMDocument.3.0":
+					this.createElement = ProxiedVirtualDOMTag;
+					break;
 				default:
 					console.log('!!!');
 					console.log(`Unknown ActiveXObject ${name}`);
 					console.log('!!!');
-					{
-						console.log("Simulating a DOM...");
-						this.createElement = ProxiedVirtualDOMTag;
-					}
 					break;
 			}
 		}
-	}, v8Context, {
+	}, {
 		timeout: 10000
-	}, function(context) {
-		self.context = context;
 	});
 }
 
 function VirtualDOMTag(name) {
-	console.log(name);
+	//console.log(`Creating a <${name}> tag...`);
 	this.name = name;
-	this.dataType = "";
-	this.nodeTypedValue = "bait";
 	return this;
 }
 
+// Catches requests to <tag>.nodeTypedValue in order to emulate them correctly
 function ProxiedVirtualDOMTag(name) {
 	return new Proxy(new VirtualDOMTag(name), {
 		get: function(target, name, receiver) {
-			console.log(`Getting ${name}...`);
+			//console.log(`Getting VirtualDOMTag.${name}`);
+			if (!(name in target)) {
+				//console.log("Not implemented!")
+			}
 			switch (name) {
 				case "nodeTypedValue":
 					return target.text;
 				default:
+					return target[name];
+			}
+		}
+	})
+}
+
+function ADODBStream() {
+	this.buffer = "";
+	this.open = () => {}
+	this.Open = this.open;
+	var resourcename = uuid.v4();
+	this.Write = this.write = function(chunk) {
+		this.buffer += String(chunk);
+	}
+	this.SaveToFile = this.saveToFile = function(filename) {
+		this.virtual_filename = filename;
+	}
+	this.Close = this.close = () => {
+		resources[resourcename] = {as: this.virtual_filename};
+		console.log("ADODB stream created:", resourcename);
+		fs.writeFileSync(resourcename, this.buffer);
+	}
+}
+
+function ProxiedADODBStream() {
+	return new Proxy(new ADODBStream(), {
+		get: function(target, name, receiver) {
+			//console.log(`Getting ProxiedADODBStream.${name}`);
+			switch (name) {
+				case "size":
+					return target.buffer.length;
+				default:
+					if (!(name in target)) {
+						//console.log("Not implemented!")
+					}
+					return target[name];
+			}
+		}
+	})
+}
+
+function XMLHTTP() {
+	this.open = function(method, url) {
+		console.log("Request made:", method, url);
+		urls.push(url);
+		this.url = url;
+	}
+	this.setRequestHeader = function(key, val) {
+		console.log("Header set:", key, val);
+	}
+	this.send = function(data) {
+		if (data)
+			console.log("Data sent:", data);
+		this.readyState = 4;
+		this.status = 200;
+		this.ResponseBody = `(Content of ${this.url})`;
+		this.onreadystatechange();
+	}
+}
+
+function ProxiedXMLHTTP() {
+	return new Proxy(new XMLHTTP(), {
+		get: function(target, name, receiver) {
+			//console.log(`Getting XMLHTTP.${name}`);
+			switch (name) {
+				default:
+					if (!(name in target)) {
+						//console.log("Not implemented!")
+					}
 					return target[name];
 			}
 		}
