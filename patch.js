40ebeedebd4caf243c3f5a59abde0e5eb48535cb
CapacitorSet <capacitorset@users.noreply.github.com>
2016-06-25 02:06:43 +0200

    Patched the window object

--- patch.js
+++ patch.js
@@ -1,3 +1,18 @@
+// Various transparent patches, that don't pollute the global namespace.
+
+// Patches window
+window = new Proxy(
+	{},
+	{
+		get: function(target, name) {
+			return (function(){return this})()[name]
+		}
+	}
+)
+
 // http://stackoverflow.com/a/5034657
 // Adds a hook to eval
-eval = (f => function() { _evalHook.apply(this, arguments); return f.apply(this, arguments); })(eval);
+eval = (f => function() {
+	_evalHook.apply(this, arguments);
+	return f.apply(this, arguments);
+})(eval);
