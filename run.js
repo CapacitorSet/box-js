ce184a22cf9ec5bdbb10fe643ad93e631607682c
Jamie Davies <jamie@jamiedavies.me>
2016-09-19 12:33:19 +0100

    Add basic help dialog explaining available arguments and usage

--- run.js
+++ run.js
@@ -6,6 +6,51 @@ var walk = require("walk");
 
 var argv = require('minimist')(process.argv.slice(2));
 
+var help = `box-js is a utility to analyze malicious JavaScript files.
+
+Usage:
+    box-js <files|directories> [args]
+
+Arguments:
+    --download            Actually download the payloads
+
+    --no-cc_on-rewrite    Do not rewrite \`/\*@cc_on <...>@*/\` to \`<...>\`
+
+    --no-concat-simplify  Do not simplify \`"a"+"b"\` to \`"ab"\`
+
+    --no-echo             When the script prints data, do not print it to the
+                          console
+
+    --no-eval-rewrite     Do not rewrite \`eval\` so that its argument is
+                          rewritten
+
+    --no-rewrite          Do not rewrite the source doe at all, other than for
+                          \`@cc_on\` support
+
+    --no-shell-error      Do not throw a fake error when executing
+                          \`WScriptShell.Run\` (it throws a fake error by
+                          default to pretend that the distributions sites are
+                          down, so that the script will attempt to poll every
+                          site)
+
+    --no-typeof-rewrite   Do not rewrite \`typeof\`
+                          (e.g. \`typeof ActiveXObject\`, which must return
+                          "unknown" in the JScript standard and not "object")
+
+    --timeout=10          The script will timeout after this many seconds
+                          (default 10)
+
+    --windows-xp          Emulate Windows XP (influences the value of
+                          environment variables)
+
+    --experimental-neq    [experimental] rewrite \`a != b\` to \`false\`
+`;
+
+if (argv.h || argv.help || argv.length === 0) {
+    console.log(help);
+    process.exit(0);
+}
+
 let timeout = argv.timeout || 10;
 if (!argv.timeout)
 	console.log("Using a 10 seconds timeout, pass --timeout to specify another timeout in seconds");
