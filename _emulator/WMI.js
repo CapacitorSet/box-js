61cbff065de20a733af5171b9d908dc5f35f4a7e
Capacitor Set <capacitorset@gmail.com>
2017-05-07 13:40:06 +0200

    Fix some WMI stuff

--- _emulator/WMI.js
+++ _emulator/WMI.js
@@ -1,25 +1,25 @@
 const controller = require("../_controller");
 
 function VirtualWMIObject(object) {
-	return new Proxy({}, {
+	return new Proxy(object, {
 		get: function(target, name) {
 			name = name.toLowerCase();
 			switch (name) {
 				case "instancesof":
 					return function(table) {
 						table = table.toLowerCase();
-						if (object[table]) return object[table];
+						if (target[table]) return target[table];
 						switch (table) {
 							default:
 								if (!(name in target)) {
-									controller.kill(`WMIObject(${object}).InstancesOf(${table}) not implemented!`);
+									controller.kill(`WMIObject(${JSON.stringify(target)}).InstancesOf(${table}) not implemented!`);
 								}
 								return target[name];
 						}
 					};
 				default:
 					if (!(name in target)) {
-						controller.kill(`WMIObject(${object}).${name} not implemented!`);
+						controller.kill(`WMIObject(${JSON.stringify(target)}).${name} not implemented!`);
 					}
 					return target[name];
 			}
@@ -33,15 +33,15 @@ module.exports.GetObject = function(name) {
 	switch (name) {
 		case "winmgmts:":
 			return VirtualWMIObject({
-				"win32_computersystemproduct": [{
+				"win32_computersystemproduct": [VirtualWMIObject({
 					name: "Foobar",
-				}],
-				"win32_operatingsystem": [{
+				})],
+				"win32_operatingsystem": [VirtualWMIObject({
 					caption: "Microsoft Windows 10 Pro",
-				}],
-				"win32_logicaldisk": [{ // dirty patch by @ALange
+				})],
+				"win32_logicaldisk": [VirtualWMIObject({ // dirty patch by @ALange
 					volumeserialnumber: "B55B4A40",
-				}],
+				})],
 			});
 		case "winmgmts:\\\\localhost\\root\\securitycenter":
 			return VirtualWMIObject({
