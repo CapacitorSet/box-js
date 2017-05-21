523885c7e6b3ae399f8709850c48426ab7822c74
Hynek Petrak <hynek.petrak@gmail.com>
2017-05-21 22:37:45 +0200

    retab :-|

--- emulator/WScriptShell.js
+++ emulator/WScriptShell.js
@@ -51,29 +51,29 @@ function WScriptShell() {
 		if (process.argv.indexOf("--no-shell-error") === -1)
 			throw new Error("If you can read this, re-run box.js with the --no-shell-error flag.");
 	};
-    this._reg_entries = {
+	this._reg_entries = {
 			"HKLM\\SOFTWARE\\MICROSOFT\\WINDOWS NT\\CURRENTVERSION\\CURRENTVERSION":"5.1",
 			"HKLM\\SOFTWARE\\MICROSOFT\\WINDOWS NT\\CURRENTVERSION\\SYSTEMROOT":"C:\\WINDOWS"
-    }
-    this._normalize_reg_key = (key) => {
+	}
+	this._normalize_reg_key = (key) => {
 		key = key.toUpperCase().replace("HKEY_LOCAL_MACHINE", "HKLM");
 		key = key.replace("HKEY_CLASSES_ROOT", "HKCR").replace("HKEY_USERS", "HKU");
 		key = key.replace("HKEY_CURRENT_USER", "HKCU").replace("HKEY_CURRENT_CONFIG", "HKCC");
-        return key;
-    }
+		return key;
+	}
 	this.regread = (key) => {
-        key = this._normalize_reg_key(key);
+		key = this._normalize_reg_key(key);
 		console.log(`Reading registry key ${key}`);
-        if (!(key in this._reg_entries))
-            return this._reg_entries[key];
-        console.log("Unknown registry key!");
-        return "";
+		if (!(key in this._reg_entries))
+			return this._reg_entries[key];
+		console.log("Unknown registry key!");
+		return "";
 	};
 	this.regwrite = (key, value, type = "(unspecified)") => {
-        key = this._normalize_reg_key(key);
-        console.log(`Setting registry key ${key} to ${value} of type ${type}`);
-        this._reg_entries[key] = value;
-    }
+		key = this._normalize_reg_key(key);
+		console.log(`Setting registry key ${key} to ${value} of type ${type}`);
+		this._reg_entries[key] = value;
+	}
 	this.popup = function(text, a, title = "[Untitled]", b) {
 		if (process.argv.indexOf("--no-echo") === -1) {
 			console.log(`Script opened a popup window: title "${title}", text "${text}"`);
@@ -104,3 +104,4 @@ module.exports = function(name) {
 		},
 	});
 };
+/* vim: set autoindent noexpandtab tabstop=4 shiftwidth=4 */
