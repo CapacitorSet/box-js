1e8773fd4b95f8c68620f926421da840056fad90
Hynek Petrak <hynek.petrak@gmail.com>
2017-05-21 22:44:10 +0200

    satisfy lint, crazy

--- emulator/WScriptShell.js
+++ emulator/WScriptShell.js
@@ -52,15 +52,15 @@ function WScriptShell() {
 			throw new Error("If you can read this, re-run box.js with the --no-shell-error flag.");
 	};
 	this._reg_entries = {
-			"HKLM\\SOFTWARE\\MICROSOFT\\WINDOWS NT\\CURRENTVERSION\\CURRENTVERSION":"5.1",
-			"HKLM\\SOFTWARE\\MICROSOFT\\WINDOWS NT\\CURRENTVERSION\\SYSTEMROOT":"C:\\WINDOWS"
-	}
+		"HKLM\\SOFTWARE\\MICROSOFT\\WINDOWS NT\\CURRENTVERSION\\CURRENTVERSION": "5.1",
+		"HKLM\\SOFTWARE\\MICROSOFT\\WINDOWS NT\\CURRENTVERSION\\SYSTEMROOT": "C:\\WINDOWS",
+	};
 	this._normalize_reg_key = (key) => {
 		key = key.toUpperCase().replace("HKEY_LOCAL_MACHINE", "HKLM");
 		key = key.replace("HKEY_CLASSES_ROOT", "HKCR").replace("HKEY_USERS", "HKU");
 		key = key.replace("HKEY_CURRENT_USER", "HKCU").replace("HKEY_CURRENT_CONFIG", "HKCC");
 		return key;
-	}
+	};
 	this.regread = (key) => {
 		key = this._normalize_reg_key(key);
 		console.log(`Reading registry key ${key}`);
@@ -73,7 +73,7 @@ function WScriptShell() {
 		key = this._normalize_reg_key(key);
 		console.log(`Setting registry key ${key} to ${value} of type ${type}`);
 		this._reg_entries[key] = value;
-	}
+	};
 	this.popup = function(text, a, title = "[Untitled]", b) {
 		if (process.argv.indexOf("--no-echo") === -1) {
 			console.log(`Script opened a popup window: title "${title}", text "${text}"`);
