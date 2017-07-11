46904f3185960a637d9315320a252d43b7df0d00
Capacitor Set <capacitorset@gmail.com>
2017-07-11 16:25:28 +0200

    Fix Dictionary to use the correct property

--- emulator/Dictionary.js
+++ emulator/Dictionary.js
@@ -4,8 +4,8 @@ function Dictionary() {
 	this.dictionary = {};
 	/* eslint no-return-assign: 0 */
 	// See https://github.com/eslint/eslint/issues/7285
-	this.add = (key, value) => (this[key] = value);
-	this.item = (key) => this[key];
+	this.add = (key, value) => (this.dictionary[key] = value);
+	this.item = (key) => this.dictionary[key];
 }
 
 module.exports = function() {
