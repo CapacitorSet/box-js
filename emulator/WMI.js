ab39a4b2db5aeaeda543bb4d009b1a36e2220080
Capacitor Set <capacitorset@gmail.com>
2017-07-31 19:36:27 +0200

    Ignore namespaces in WMI (fixes #22)

--- emulator/WMI.js
+++ emulator/WMI.js
@@ -3,7 +3,7 @@ const lib = require("../lib");
 function VirtualWMIObject(object) {
 	return new Proxy(object, {
 		get: function(target, name) {
-			name = name.toLowerCase();
+			if (typeof name === "string") name = name.toLowerCase();
 			switch (name) {
 				case "instancesof":
 					return function(table) {
@@ -11,8 +11,18 @@ function VirtualWMIObject(object) {
 						if (table in target) return target[table];
 						lib.kill(`WMIObject(${JSON.stringify(target)}).InstancesOf(${table}) not implemented!`);
 					};
+				case "execquery":
+					return query => {
+						query = query.toLowerCase();
+						// TODO: implement actual SQL
+						if (query === "select * from win32_process") return [{name: "wscript.exe"}];
+						if (query === "select version from win32_operatingsystem") return [{version: "10"}];
+						throw new Error(`Not implemented: query "${query}"`);
+					}
 				default:
 					if (name in target) return target[name];
+					if (name === "valueof") return undefined;
+					if (name === "tojson") return () => JSON.stringify(target);
 					lib.kill(`WMIObject(${JSON.stringify(target)}).${name} not implemented!`);
 			}
 		},
@@ -20,31 +30,30 @@ function VirtualWMIObject(object) {
 }
 
 module.exports.GetObject = function(name) {
+/*
 	name = name.toLowerCase();
 	name = name.replace(/{impersonationlevel=impersonate}/g, "");
 	switch (name) {
 		case "winmgmts:":
-			return new VirtualWMIObject({
-				"win32_computersystemproduct": [new VirtualWMIObject({
-					name: "Foobar",
-				})],
-				"win32_operatingsystem": [new VirtualWMIObject({
-					caption: "Microsoft Windows 10 Pro",
-				})],
-				"win32_logicaldisk": [new VirtualWMIObject({ // dirty patch by @ALange
-					deviceid: "C:",
-					volumeserialnumber: "B55B4A40",
-				})],
-			});
+			// ...
 		case "winmgmts:\\\\localhost\\root\\securitycenter":
-			return new VirtualWMIObject({
-				"antivirusproduct": [],
-			});
 		case "winmgmts:\\\\localhost\\root\\securitycenter2":
-			return new VirtualWMIObject({
-				"antivirusproduct": [],
-			});
+			// ...
 		default:
 			lib.kill(`GetObject(${name}) not implemented!`);
 	}
+*/
+	return new VirtualWMIObject({
+		"win32_computersystemproduct": [new VirtualWMIObject({
+			name: "Foobar",
+		})],
+		"win32_operatingsystem": [new VirtualWMIObject({
+			caption: "Microsoft Windows 10 Pro",
+		})],
+		"win32_logicaldisk": [new VirtualWMIObject({ // dirty patch by @ALange
+			deviceid: "C:",
+			volumeserialnumber: "B55B4A40",
+		})],
+		"antivirusproduct": [],
+	});
 };
