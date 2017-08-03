e60c9023d21ff83c027e0158b0dc008d3bd9ad76
Capacitor Set <capacitorset@gmail.com>
2017-08-03 17:32:52 +0200

    Fix Function patch

--- patch.js
+++ patch.js
@@ -17,7 +17,12 @@
 
 	_OriginalFunction = Function;
 	Function = function(...args) {
-		const source = rewrite(args.pop());
+		const originalSource = args.pop();
+		/* Wrap the original source in an IIFE so that uglify-js doesn't
+		 * complain about return statements outside of a function
+		 */
+		const IIFE = `(() => {${originalSource}})()`;
+		const source = "/* IIFE added by box-js, see patch.js */ return " + rewrite(IIFE);
 		logJS(source);
 		return new _OriginalFunction(...args, source);
 	}
