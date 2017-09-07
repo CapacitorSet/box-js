b0647ff576389cff9d19d09199edcd2b21684ce1
Capacitor Set <capacitorset@gmail.com>
2017-09-07 18:13:52 +0200

    Add Cuckoo URLs support

--- export.js
+++ export.js
@@ -4,9 +4,6 @@ const RateLimiter = require('limiter').RateLimiter;
 const syncRequest = require("sync-request");
 const walk = require("walk-sync");
 
-let virustotalRateLimit = argv["vt-rate-limit"] || 4; // The public API is limited to 4 requests per minute.
-const virustotalLimiter = new RateLimiter(virustotalRateLimit, "minute");
-
 function request(method, URL, params) {
 	if (method === "GET")
 		URL += "?" + Object.keys(params)
@@ -101,7 +98,10 @@ Array.prototype.functionalSplit = function(f) {
 Array.prototype.flatten = function() {
 	return this.reduce((a, b) => a.concat(b), []);
 }
-Array.prototype.unique = function() {
+Array.prototype.objectFlatten = function() {
+	return this.reduce((a, b) => Object.assign({}, a, b), {});
+};
+Array.prototype.uniqueStrings = function() {
 	// O(n) check using an object as hash map
 	// Works for arrays of strings, not guaranteed to work for anything else
 	let unique = {};
@@ -109,6 +109,16 @@ Array.prototype.unique = function() {
 		unique[elem] = null;
 	return Object.keys(unique);
 }
+Array.prototype.unique = function(comparator = (a, b) => a === b) {
+	if (this.every(item => typeof item === "string")) // Optimize: O(n) rather than O(n^2)
+		return this.uniqueStrings();
+	return this.reduce(
+		(uniq, item) => uniq.some(x => comparator(x, item))
+			? uniq
+			: [...uniq, item],
+		[]
+	);
+};
 
 const args = process.argv.slice(2);
 
@@ -132,36 +142,101 @@ if (folders.length === 0) {
 	process.exit(-1);
 }
 
-log("info", "Parsing URLs...");
-const urls = folders
+let urls, numUrls;
+if (argv["cuckoo-urls"] || argv["vt-urls"]) {
+	log("info", "Parsing URLs...");
+	urls = folders
+		.filter(item => {
+			if (!fs.existsSync(item + "/urls.json")) {
+				log("verb", `URL collection: discarding ${item} because it doesn't contain urls.json`);
+				return false;
+			}
+			log("debug", `URL collection: folder ${item} is valid.`);
+			return true;
+		})
+		.map(item => JSON.parse(fs.readFileSync(item + "/urls.json")))
+		.flatten()
+		.unique();
+	numUrls = urls.length;
+}
+
+let cuckooAddress;
+if (argv["cuckoo-urls"] || argv["cuckoo-all-files"] || argv["cuckoo-executables"]) {
+	if (!argv["cuckoo-address"])
+		throw new Error("Please enter a valid Cuckoo address.");
+	cuckooAddress = argv["cuckoo-address"];
+	if (!/^http/i.test(cuckooAddress))
+		cuckooAddress = "http://" + cuckooAddress;
+}
+
+let malwrLimiter;
+if (argv["malwr-all-files"] || argv["malwr-executables"]) {
+	if (!argv["malwr-key"])
+		throw new Error("Please enter a valid API key for Malwr.");
+	// Malwr doesn't enforce rate limits, but let's be nice
+	malwrLimiter = new RateLimiter(1, "second");
+}
+
+let vtRateLimit, vtLimiter;
+if (argv["vt-urls"] || argv["vt-all-files"] || argv["vt-executables"]) {
+	// The public API is limited to 4 requests per minute.
+	vtRateLimit = argv["vt-rate-limit"] || 4;
+	vtLimiter = new RateLimiter(vtRateLimit, "minute");
+	if (!argv["vt-key"])
+		throw new Error("Please enter a valid API key for VirusTotal.");
+}
+
+if (argv["cuckoo-urls"]) {
+	log("info", `Submitting ${numUrls} URLs to Cuckoo`);
+	let cuckooSubmittedUrls = 0;
+	for (const url of urls) {
+		request("POST", cuckooAddress + "/tasks/create/url", {
+			url
+		});
+		cuckooSubmittedUrls++;
+		log("info", `URLs submitted to Cuckoo: ${cuckooSubmittedUrls}/${numUrls} (${(100 * cuckooSubmittedUrls/numUrls).toFixed(2)}%)`);
+	}
+}
+
+if (argv["vt-urls"]) {
+	log("info", `Submitting ${numUrls} URLs to VirusTotal`);
+	let vtSubmittedUrls = 0;
+	// If the requests can be sent without hitting the rate limit, do not print progress info
+	const quiet = numUrls <= vtRateLimit;
+	if (!quiet)
+		log("info", `Due to API limits, this will take ${(numUrls / vtRateLimit - 1).toFixed(1)} minutes.`);
+	for (const url of urls)
+		vtLimiter.removeTokens(1, () => {
+			request("GET", "http://localhost:8000/test", {
+				apikey: argv["vt-key"],
+				url
+			});
+			vtSubmittedUrls++;
+			if (!quiet)
+				log("info", `URLs submitted to VirusTotal: ${vtSubmittedUrls}/${numUrls} (${(100 * vtSubmittedUrls/numUrls).toFixed(2)}%)`);
+		});
+}
+
+/*
+const allFiles = folders
 	.filter(item => {
-		if (!fs.existsSync(item + "/urls.json")) {
-			log("verb", `URL collection: discarding ${item} because it doesn't contain urls.json`);
+		if (!fs.existsSync(item + "/resources.json")) {
+			log("verb", `URL collection: discarding ${item} because it doesn't contain resources.json`);
 			return false;
 		}
 		log("debug", `URL collection: folder ${item} is valid.`);
 		return true;
 	})
-	.map(item => JSON.parse(fs.readFileSync(item + "/urls.json")))
-	.flatten()
-	.unique();
-
-if (argv["vt-urls"]) {
-	const numUrls = urls.length;
-	let submitted = 0;
-	log("info", `Submitting ${numUrls} URLs to VirusTotal`);
-	// If the requests can be sent without hitting the rate limit, do not print progress info
-	const quiet = numUrls <= virustotalRateLimit;
-	if (!quiet)
-		log("info", `Due to API limits, this will take ${(numUrls / virustotalRateLimit - 1).toFixed(1)} minutes.`);
-	for (const url of urls)
-		virustotalLimiter.removeTokens(1, () => {
-			request("GET", "http://localhost:8000/test", {
-				apikey: argv["vt-key"],
-				url
-			});
-			submitted++;
-			if (!quiet)
-				log("info", `URLs submitted to VirusTotal: ${submitted}/${numUrls} (${(100 * submitted/numUrls).toFixed(2)}%)`);
-		});
-}
+	.map(item => fs.readFileSync(item + "/resources.json", "utf8"))
+	.map(x => {
+		try {
+			return JSON.parse(x);
+		} catch (e) {
+			console.log(x);
+			console.log(e);
+			process.exit(1);
+		}
+	})
+	.objectFlatten()
+	.unique((a, b) => a.sha256 === b.sha256);
+*/
