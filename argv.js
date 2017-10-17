a86569b0730493ef663ef0fb593fee63a8ef5799
Florian Beeres <fbeeres@gmail.com>
2017-10-17 14:45:27 +0200

    Throw error on unknown arguments
    commandLineArgs already recognizes these unknown arguments so there is not a lot to do here. I added pluralize for a nicer error message.
    
    Added tags to .gitignore for people like me who create & use ctags files in vim.

--- argv.js
+++ argv.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const commandLineArgs = require("command-line-args");
+const pluralize = require("pluralize");
 
 const flags = JSON.parse(fs.readFileSync(path.join(__dirname, "flags.json"), "utf8"));
 
@@ -12,8 +13,11 @@ function getArgs(flags) {
 		return flag;
 	});
 	const argv = commandLineArgs(flags, {
-		partial: true
+		partial: true,
 	});
+	if (argv._unknown && argv._unknown.length) {
+		throw new Error(`Unknown ${pluralize("argument", argv._unknown.length)}: ${argv._unknown}`);
+	}
 	if (!argv.loglevel) argv.loglevel = "info"; // The default value handling in the library is buggy
 	if (argv.loglevel === "verbose") argv.loglevel = "verb";
 	if (argv.loglevel === "warning") argv.loglevel = "warn";
