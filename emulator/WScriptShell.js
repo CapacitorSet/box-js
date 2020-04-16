403893c7585d26c543c81030f149697ee965428c
Connor <connorshride2@gmail.com>
2020-04-16 15:22:47 -0500

    registry read, write, delete

--- emulator/WScriptShell.js
+++ emulator/WScriptShell.js
@@ -3,120 +3,220 @@ const TextStream = require("./TextStream.js");
 const argv = require("../argv.js").run;
 
 function WScriptShell() {
+
     const vars = {
-	/* %APPDATA% equals C:\Documents and Settings\{username}\Application Data on Windows XP,
-	 * but C:\Users\{username}\AppData\Roaming on Win Vista and above.
-	 */
-	appdata: argv["windows-xp"]
-	    ? "C:\\Documents and Settings\\User\\Application Data"
-	    : "C:\\Users\\User\\AppData\\Roaming",
-	computername: "USER-PC",
-	comspec: "%SystemRoot%\\system32\\cmd.exe",
-	os: "Windows_NT",
-	processor_revision: "0209",
-	processor_architecture: "x86",
-	programdata: "C:\\ProgramData",
-	systemroot: "C:\\WINDOWS",
-	//tmp: "C:\\DOCUME~1\\User\\LOCALS~1\\Temp",
-        tmp: "C:\\Users\\SYSOP1~1\\AppData\\Local\\Temp",
-	//temp: "C:\\DOCUME~1\\User\\LOCALS~1\\Temp",
-        temp: "C:\\Users\\SYSOP1~1\\AppData\\Local\\Temp",
-	username: "User",
-	userprofile: "C:\\Users\\Sysop12\\",
-	windir: "C:\\WINDOWS"
-    };
+		/* %APPDATA% equals C:\Documents and Settings\{username}\Application Data on Windows XP,
+		* but C:\Users\{username}\AppData\Roaming on Win Vista and above.
+		*/
+		appdata: argv["windows-xp"]
+			? "C:\\Documents and Settings\\User\\Application Data"
+			: "C:\\Users\\User\\AppData\\Roaming",
+		computername: "USER-PC",
+		comspec: "%SystemRoot%\\system32\\cmd.exe",
+		os: "Windows_NT",
+		processor_revision: "0209",
+		processor_architecture: "x86",
+		programdata: "C:\\ProgramData",
+		systemroot: "C:\\WINDOWS",
+		//tmp: "C:\\DOCUME~1\\User\\LOCALS~1\\Temp",
+		tmp: "C:\\Users\\SYSOP1~1\\AppData\\Local\\Temp",
+		//temp: "C:\\DOCUME~1\\User\\LOCALS~1\\Temp",
+		temp: "C:\\Users\\SYSOP1~1\\AppData\\Local\\Temp",
+		username: "User",
+		userprofile: "C:\\Users\\Sysop12\\",
+		windir: "C:\\WINDOWS"
+	};
+	
     this.environment = (x) => {
-	if (x.toLowerCase() === "system")
-	    return (argument) => {
-		argument = argument.toLowerCase();
-		if (argument in vars) return vars[argument];
-		lib.kill(`Unknown parameter ${argument} for WScriptShell.Environment.System`);
-	    };
-        if (x.toLowerCase() === "process")
-            return {
-                Item: function(x) {
-                    if (x.toLowerCase() === "programdata")
-                        return "C:\\ProgramData";
-                    return "Unknown process item " + x;
-                }
-            };
-	return `(Environment variable ${x})`;
-    };
+		if (x.toLowerCase() === "system")
+			return (argument) => {
+				argument = argument.toLowerCase();
+				if (argument in vars) return vars[argument];
+				lib.kill(`Unknown parameter ${argument} for WScriptShell.Environment.System`);
+			};
+			if (x.toLowerCase() === "process")
+				return {
+					Item: function(x) {
+						if (x.toLowerCase() === "programdata")
+							return "C:\\ProgramData";
+						return "Unknown process item " + x;
+					}
+				};
+		return `(Environment variable ${x})`;
+	};
+	
     this.environment1 = undefined;
     this.specialfolders = (x) => `(Special folder ${x})`;
     this.createshortcut = () => ({});
-    this.popup = (text, wait=0, title=0, type=0) => console.log("Display popup'" + text + "'");
+	this.popup = (text, wait=0, title=0, type=0) => console.log("Display popup'" + text + "'");
     this.expandenvironmentstrings = (path) => {
-	Object.keys(vars).forEach(key => {
-	    const regex = RegExp("%" + key + "%", "gi");
-	    if (!regex.test(path)) return;
-	    lib.info(`Script read environment variable ${key}`);
-	    path = path.replace(regex, vars[key]);
-	});
+		Object.keys(vars).forEach(key => {
 
-	if (/%\w+%/i.test(path)) {
-	    lib.warning("Possibly failed to expand environment strings in " + path);
-	}
+			const regex = RegExp("%" + key + "%", "gi");
 
-	return path;
-    };
-    this.run = cmd => {
-	lib.runShellCommand(cmd);
-	return 0;
-    };
-    this.exec = cmd => {
-	lib.runShellCommand(cmd);
-	return {
-	    ExitCode: 1,
-	    ProcessID: Math.floor(Math.random() * 1000),
-	    Status: 1, // Finished			
-	    StdErr: null,
-	    StdIn: null,
-	    StdOut: new TextStream(`<output of ${cmd}>`),
+			if (!regex.test(path)) return;
+
+			lib.info(`Script read environment variable ${key}`);
+			path = path.replace(regex, vars[key]);
+		});
+
+		if (/%\w+%/i.test(path)) {
+			lib.warning("Possibly failed to expand environment strings in " + path);
+		}
+
+		return path;
 	};
-    };
-    this._normalize_reg_key = (key) => {
-	key = key
-	    .replace("HKLM", "HKEY_LOCAL_MACHINE")
-	    .replace("HKCR", "HKEY_CLASSES_ROOT")
-	    .replace("HKU", "HKEY_USERS")
-	    .replace("HKCU", "HKEY_CURRENT_USER")
-	    .replace("HKCC", "HKEY_CURRENT_CONFIG");
-	return key;
-    };
-    this.regread = (key) => {
-	if (!this._reg_entries) // Load once needed
-	    this._reg_entries = require("system-registry");
-	key = this._normalize_reg_key(key);
-	lib.logIOC("RegRead", {key}, "The script read a registry key.");
-	lib.verbose(`Reading registry key ${key}`);
+	
+    this.run = cmd => {
+		lib.runShellCommand(cmd);
+		return 0;
+	};
+	
+    this.exec = cmd => {
+		lib.runShellCommand(cmd);
+		return {
+			ExitCode: 1,
+			ProcessID: Math.floor(Math.random() * 1000),
+			Status: 1, // Finished			
+			StdErr: null,
+			StdIn: null,
+			StdOut: new TextStream(`<output of ${cmd}>`),
+		};
+	};
+	
+	if (!this._reg_entries)
+		this._reg_entries = require("system-registry");
 
-	const normalizedEqual = (a, b) => a.toLowerCase() === b.toLowerCase();
-	let val = this._reg_entries;
-	const parts = key.split("\\");
-	for (part of parts) {
-	    if (!Object.keys(val).some(key => normalizedEqual(key, part))) {
-		lib.warning(`Unknown registry key ${key}!`);
-		return "";
-	    }
-	    // De-normalization: retrieve the key with the actual capitalization
-	    const actualKey = Object.keys(val).filter(key => normalizedEqual(key, part))[0]
-	    val = val[actualKey];
+		// lacks the HKEY_CURRENT_USER reg key by default (y tho?)
+		this._reg_entries["HKEY_CURRENT_USER"] = {}
+
+	// expand registry acronyms and make lowercase
+    function normalizeRegKey(key) {
+		key = key
+			.replace("HKLM", "HKEY_LOCAL_MACHINE")
+			.replace("HKCR", "HKEY_CLASSES_ROOT")
+			.replace("HKU", "HKEY_USERS")
+			.replace("HKCU", "HKEY_CURRENT_USER")
+			.replace("HKCC", "HKEY_CURRENT_CONFIG");
+		return key.toLowerCase();
+	};
+	
+	// traverse registry object searching for the key
+	this._resolveRegKey = (inKey) => {
+
+		var inKeyParts = inKey.split("\\")
+		var currRegEntry = this._reg_entries
+
+		// compare the given key to the "this" value (see usage below)
+		var keysEqual = function(key) {
+			return normalizeRegKey(key) === normalizeRegKey(this)
+		}
+
+		for (inKeyPart of inKeyParts) {
+
+			// give the part of the input key we're searching for as the "this" value of keysEqual
+			var foundKey = Object.keys(currRegEntry).filter(keysEqual, inKeyPart)
+			if (foundKey.length > 0) {
+				currRegEntry = currRegEntry[foundKey[0]]
+			}
+			else {
+				return undefined
+			}
+		}
+
+		return currRegEntry
 	}
-	return val;
-    };
+
+    this.regread = (key) => {
+
+		// log the IOC whether or not we handle the read correctly
+		lib.logIOC("RegRead", {key}, "The script read a registry key");
+		value = this._resolveRegKey(key)
+
+		if (value) {
+			lib.verbose(`Read registry key ${key}`);
+			return value
+		}
+		else {
+			lib.warning(`Unknown registry key ${key}!`);
+			return "";
+		}
+	};
+	
     this.regwrite = (key, value, type = "(unspecified)") => {
-	key = this._normalize_reg_key(key);
-	lib.logIOC("RegWrite", {key, value, type}, "The script wrote a registry key.");
-	lib.info(`Setting registry key ${key} to ${value} of type ${type}`);
-	this._reg_entries[key] = value;
-    };
-    this.popup = function(text, a, title = "[Untitled]", b) {
-	if (!argv["no-echo"]) {
-	    lib.verbose(`Script opened a popup window: title "${title}", text "${text}"`);
-	    lib.verbose("Add flag --no-echo to disable this.");
+
+		// log the IOC whether or not we correctly handle it
+		lib.logIOC("RegWrite", {key, value, type}, "The script wrote to a registry key");
+
+		var badKey = false
+		var existingKey = key
+		var existingRegEntry = undefined
+		var keysToCreate = []
+
+		// find the deepest part of the given key that exists in our registry object
+		do {
+			existingRegEntry = this._resolveRegKey(existingKey)
+
+			// if we've checked the very top level key and didn't find it
+			if (existingKey.split("\\").length == 1 && !existingRegEntry) {
+				lib.info("script tried to write to an invalid key root " + existingKey)
+				badKey = true
+			}
+
+			// chop off the last element of the key path and try again
+			// save the last part of the key that didn't exist as the key we need to create
+			if (!existingRegEntry && !badKey) {
+				keyParts = existingKey.split("\\")
+				keysToCreate.unshift(keyParts.pop())
+				existingKey = keyParts.join("\\")
+			}
+		} while (!existingRegEntry && !badKey);
+
+		if (!badKey) {
+			// the key already existed, just need to overwrite the last element
+			if (keysToCreate.length == 0) {
+				keysToCreate.unshift(key.split("\\").pop())
+			}
+
+			lib.info(`Setting registry key ${key} to ${value} of type ${type}`);
+
+			// iterate through keys that need new nested objects
+			while (keysToCreate.length > 1) {
+				newKey = keysToCreate.shift()
+				existingRegEntry[newKey] = {}
+				existingRegEntry = existingRegEntry[newKey]
+			}
+
+			// set the value in our (possibly) newly created registry entry
+			existingRegEntry[keysToCreate.shift()] = value
+		}
+	};
+	
+	this.regdelete = (key) => {
+
+		lib.logIOC("RegDelete", {key}, "The script deleted a registry key.");
+
+		keyParts = key.split("\\")
+		keyToDelete = keyParts.pop()
+		pathtoKey = keyParts.join("\\")
+
+		toDelete = this._resolveRegKey(pathtoKey)
+
+		if (toDelete) {
+			lib.info(`deleting registry key ${key}`);
+			delete toDelete[keyToDelete]
+		}
+		else {
+			lib.warning(`registry key not present ${key}`)
+		}
 	}
-	return true; // Emulates a click
+
+    this.popup = function(text, a, title = "[Untitled]", b) {
+		if (!argv["no-echo"]) {
+			lib.verbose(`Script opened a popup window: title "${title}", text "${text}"`);
+			lib.verbose("Add flag --no-echo to disable this.");
+		}
+		return true; // Emulates a click
     };
 }
 
