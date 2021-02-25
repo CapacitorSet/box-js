f045334a48b32205ff3e316a69ff8a4a040cb163
Kirk.Sayre <Kirk.Sayre@walmart.com>
2021-02-25 17:08:27 -0600

    Track dynamically run ActiveX code as an IOC.

--- emulator/MSScriptControlScriptControl.js
+++ emulator/MSScriptControlScriptControl.js
@@ -7,6 +7,7 @@ function ScriptControl() {
 
     this.addcode = code => {
         lib.info(`Script added dynamic code '''${code}'''`);
+        lib.logIOC("DynamicCode", {code}, "The script wrote dynamic code with MSScriptControl.ScriptControl ActiveX object.");
 	return 0;
     };
 }
